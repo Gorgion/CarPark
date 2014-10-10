d07a0ca0073a3807cfab8560fa356d0ca8555cfe
KB9 <karolinaburska9@gmail.com>
2014-10-10 20:35:32 +0200

    OfficeDaoImpl and CarDaoTest updated

--- carpark-backend/src/main/java/cz/muni/fi/pa165/carpark/dao/OfficeDaoImpl.java
+++ carpark-backend/src/main/java/cz/muni/fi/pa165/carpark/dao/OfficeDaoImpl.java
@@ -8,222 +8,219 @@ package cz.muni.fi.pa165.carpark.dao;
 import cz.muni.fi.pa165.carpark.entity.Car;
 import cz.muni.fi.pa165.carpark.entity.Office;
 import cz.muni.fi.pa165.carpark.entity.User;
-
 import java.util.Collections;
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 
-
 /**
- * Office entity with operations add, get, edit, delete.
- * It also finds all offices, office cars and office employees.
+ * Office entity with operations add, get, edit, delete. It also finds all
+ * offices, office cars and office employees.
  *
  * @author Karolina Burska
  */
 public class OfficeDaoImpl implements OfficeDao {
-    	
-        private EntityManagerFactory emf;
-        
-        @Override
-        public void setEMF(EntityManagerFactory emf){
-            this.emf = emf;
-        }
-    
-        @Override
-        public void addOffice(Office office){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            if(office.getID() != null) {
-                throw new IllegalArgumentException("Office is already added.");
-            }
 
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-            em.persist(office);
-            em.getTransaction().commit();
-            em.close();        
+    private EntityManagerFactory emf;
+
+    @Override
+    public void setEMF(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public void addOffice(Office office) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
         }
-        
-        @Override
-        public Office getOffice(Long id){
-            if(id == null) {
+        if (office.getID() != null) {
+            throw new IllegalArgumentException("Office is already added.");
+        }
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(office);
+        em.getTransaction().commit();
+        em.close();
+        System.out.println(office);
+    }
+
+    @Override
+    public Office getOffice(Long id) {
+        if (id == null) {
             throw new IllegalArgumentException("ID is null.");
-            }
-            if(id < 0) {
+        }
+        if (id <= 0) {
             throw new IllegalArgumentException("ID is less than 0.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-
-            Office office = em.find(Office.class, id);
-
-            em.getTransaction().commit();
-            em.close();
-        
-            return office;
-        }
-        
-        @Override
-	public void editOffice(Office office){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-            em.merge(office);
-            em.getTransaction().commit();
-            em.close();
         }
 
-        @Override
-	public void deleteOffice(Office office){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-            em.remove(em.find(Office.class, office.getID()));   
-            em.getTransaction().commit();
-            em.close(); 
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Office office = em.find(Office.class, id);
+
+        em.getTransaction().commit();
+        em.close();
+
+        return office;
+    }
+
+    @Override
+    public void editOffice(Office office) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
         }
 
-        @Override
-	public List<Office> getAllOffices(){
-            EntityManager em = emf.createEntityManager();
-        
-            Query query = em.createQuery("SELECT * FROM Office");
-            List<Office> offices = query.getResultList();
-        
-            em.getTransaction().commit();
-            em.close();
-        
-            return Collections.unmodifiableList(offices);
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.merge(office);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void deleteOffice(Office office) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
         }
 
-        @Override
-	public List<Car> getOfficeCars(Office office){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            Query query = em.createQuery("SELECT * FROM Car WHERE office=:office");
-            List<Car> cars = query.getResultList();
-            
-            return Collections.unmodifiableList(cars);
-        }
-        
-        @Override
-        public void addCarToOffice(Office office, Car car){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            if(car == null) {
-                throw new NullPointerException("Car is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-            
-            List<Car> actualCars = getOffice(office.getID()).getCars();
-            actualCars.add(car);
-            office.setCars(actualCars);
-            
-            em.merge(office);
-            em.getTransaction().commit();
-            em.close();
-        }
-        
-        @Override
-        public void deleteCarFromOffice(Office office, Car car){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            if(car == null) {
-                throw new NullPointerException("Car is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-            
-            List<Car> actualCars = getOffice(office.getID()).getCars();
-            actualCars.remove(car);
-            office.setCars(actualCars);
-            
-            em.merge(office);
-            em.getTransaction().commit();
-            em.close();
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.remove(em.find(Office.class, office.getID()));
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public List<Office> getAllOffices() {
+        EntityManager em = emf.createEntityManager();
+
+        Query query = em.createQuery("FROM Office");
+        List<Office> offices = query.getResultList();
+
+        em.close();
+
+        return Collections.unmodifiableList(offices);
+    }
+
+    @Override
+    public List<Car> getOfficeCars(Office office) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
         }
 
-        @Override
-	public List<User> getEmployees(Office office){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            Query query = em.createQuery("SELECT * FROM User WHERE office=:office");
-            List<User> users = query.getResultList();
-            
-            return Collections.unmodifiableList(users);
+        EntityManager em = emf.createEntityManager();
+        Query query = em.createQuery("SELECT * FROM Car WHERE office=:office");
+
+        List<Car> cars = query.getResultList();
+
+        return Collections.unmodifiableList(cars);
+    }
+
+    @Override
+    public void addCarToOffice(Office office, Car car) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
         }
-        
-        @Override
-        public void addEmployeeToOffice(Office office, User user){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            if(user == null) {
-                throw new NullPointerException("User is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-            
-            List<User> actualEmployees = getOffice(office.getID()).getEmployees();
-            actualEmployees.add(user);
-            office.setEmployees(actualEmployees);
-            
-            em.merge(office);
-            em.getTransaction().commit();
-            em.close();
+        if (car == null) {
+            throw new NullPointerException("Car is null.");
         }
-        
-        @Override
-        public void deleteEmployeeFromOffice(Office office, User user){
-            if(office == null) {
-                throw new NullPointerException("Office is null.");
-            }
-            if(user == null) {
-                throw new NullPointerException("User is null.");
-            }
-            
-            EntityManager em = emf.createEntityManager();
-            
-            em.getTransaction().begin();
-            
-            List<User> actualEmployees = getOffice(office.getID()).getEmployees();
-            actualEmployees.remove(user);
-            office.setEmployees(actualEmployees);
-            
-            em.merge(office);
-            em.getTransaction().commit();
-            em.close();
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        List<Car> actualCars = getOffice(office.getID()).getCars();
+        actualCars.add(car);
+        office.setCars(actualCars);
+
+        em.merge(office);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void deleteCarFromOffice(Office office, Car car) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
         }
+        if (car == null) {
+            throw new NullPointerException("Car is null.");
+        }
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        List<Car> actualCars = getOffice(office.getID()).getCars();
+        actualCars.remove(car);
+        office.setCars(actualCars);
+
+        em.merge(office);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public List<User> getEmployees(Office office) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
+        }
+
+        EntityManager em = emf.createEntityManager();
+
+        Query query = em.createQuery("SELECT * FROM User WHERE office=:office");
+        List<User> users = query.getResultList();
+
+        return Collections.unmodifiableList(users);
+    }
+
+    @Override
+    public void addEmployeeToOffice(Office office, User user) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
+        }
+        if (user == null) {
+            throw new NullPointerException("User is null.");
+        }
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        List<User> actualEmployees = getOffice(office.getID()).getEmployees();
+        actualEmployees.add(user);
+        office.setEmployees(actualEmployees);
+
+        em.merge(office);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void deleteEmployeeFromOffice(Office office, User user) {
+        if (office == null) {
+            throw new NullPointerException("Office is null.");
+        }
+        if (user == null) {
+            throw new NullPointerException("User is null.");
+        }
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        List<User> actualEmployees = getOffice(office.getID()).getEmployees();
+        actualEmployees.remove(user);
+        office.setEmployees(actualEmployees);
+
+        em.merge(office);
+        em.getTransaction().commit();
+        em.close();
+    }
 }
--- carpark-backend/src/test/java/cz/muni/fi/pa165/carpark/dao/CarDaoTest.java
+++ carpark-backend/src/test/java/cz/muni/fi/pa165/carpark/dao/CarDaoTest.java
@@ -5,11 +5,191 @@
  */
 package cz.muni.fi.pa165.carpark.dao;
 
+import cz.muni.fi.pa165.carpark.entity.Car;
+import cz.muni.fi.pa165.carpark.entity.Rental;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
 /**
  *
  * @author Karolina Burska
  */
 public class CarDaoTest {
     private CarDaoImpl carImpl;  
+    
+    public CarDaoTest(){  
+    }
+    
+    @Before
+    public void setup(){
+        carImpl = new CarDaoImpl();
+        carImpl.setEmf(Persistence.createEntityManagerFactory("TestPU"));
+    }
+    
+    @Test
+    public void addNewCarTest()
+    {  
+        Car car = new Car();
+        
+        car.setBrand(Car.mBrand.DAEWOO);
+        car.setColor(Car.mColor.BLUE);
+        car.setEngine(Car.mEngine.ELECTRIC);
+        car.setLicencePlate("4G5-PA165");
+        car.setModel(Car.mModel.MATIZ);
+        car.setRented(Boolean.FALSE);
+        car.setType(Car.mType.CABRIOLET);
+        car.setVIN("SomeVIN");
+        
+        carImpl.AddCar(car);
+
+        Car secondCar = carImpl.getCar(car.getID());
+
+        Assert.assertNotNull(secondCar);
+        Assert.assertEquals(car, secondCar);
+        Assert.assertNotSame(car, secondCar);
+    }
+    /*
+    @Test(expected = IllegalArgumentException.class)
+    public void addCarWithNullTest(){
+        
+    }*/
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void getCarWithWrongIdTest(){
+        carImpl.getCar(Long.getLong("-54"));
+        carImpl.getCar(Long.getLong("0"));
+        carImpl.AddCar(null);
+    }
+    
+    @Test
+    @Ignore
+    public void editCarTest(){
+        
+        Car car = new Car();
+        
+        car.setBrand(Car.mBrand.DAEWOO);
+        car.setColor(Car.mColor.BLUE);
+        car.setEngine(Car.mEngine.ELECTRIC);
+        car.setLicencePlate("4G5-PA165");
+        car.setModel(Car.mModel.MATIZ);
+        car.setRented(Boolean.FALSE);
+        car.setType(Car.mType.CABRIOLET);
+        car.setVIN("SomeVIN");
+        
+        carImpl.AddCar(car);
+        
+        Car carForEditation = new Car();
+        
+        carForEditation.setBrand(Car.mBrand.DAEWOO);
+        carForEditation.setColor(Car.mColor.BLUE);
+        carForEditation.setEngine(Car.mEngine.ELECTRIC);
+        carForEditation.setModel(Car.mModel.MATIZ);
+        carForEditation.setType(Car.mType.CABRIOLET);
+        carForEditation.setID(car.getID());
+        carForEditation.setLicencePlate("differentLicencePlate");
+        car.setRented(Boolean.FALSE);
+        carForEditation.setVIN("SomeOtherVIN");
+        
+        carImpl.EditCar(carForEditation);
+        
+        Car carForCheck = new Car();
+        
+        carForCheck.setID(car.getID());
+        
+        Assert.assertNotNull(carForCheck);
+        Assert.assertEquals(carForCheck, carForEditation);
+        Assert.assertNotSame(car, carForCheck);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void editCarWithNullTest(){
+        carImpl.EditCar(null);
+    }
+    
+    @Test
+    public void deleteCar(){
+        Car car = new Car();
+        
+        car.setBrand(Car.mBrand.DAEWOO);
+        //car.setID(Long.getLong("2"));
+        car.setColor(Car.mColor.BLUE);
+        car.setEngine(Car.mEngine.ELECTRIC);
+        car.setLicencePlate("4G5-PA165");
+        car.setModel(Car.mModel.MATIZ);
+        car.setRented(Boolean.FALSE);
+        car.setType(Car.mType.CABRIOLET);
+        car.setVIN("SomeVIN");
+        
+        carImpl.AddCar(car);
+        
+        carImpl.DeleteCar(car);
+        
+        Collection cars = carImpl.getAllCars();
+        
+        Assert.assertNotNull(cars);
+        Assert.assertTrue("Car found after deletion.", !cars.contains(car));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void deleteCarWithNullTest()
+    {
+        carImpl.DeleteCar(null);
+    }
+    
+    @Test
+    public void getAllCarsTest(){
+        
+        Car car1 = new Car();
+        car1.setBrand(Car.mBrand.CHEVROLET);
+        car1.setLicencePlate("4G5-PA161");
+        car1.setRented(Boolean.FALSE);
+        car1.setVIN("SomeVIN1");
+        
+        Car car2 = new Car();
+        car2.setBrand(Car.mBrand.FORD);
+        car2.setLicencePlate("4G5-PA162");
+        car2.setRented(Boolean.FALSE);
+        car2.setVIN("SomeVIN2");
+        
+        Car car3 = new Car();
+        car3.setBrand(Car.mBrand.TESLA);
+        car3.setLicencePlate("4G5-PA163");
+        car3.setRented(Boolean.FALSE);
+        car3.setVIN("SomeVIN3");
+        
+        carImpl.AddCar(car1);
+        carImpl.AddCar(car2);
+        carImpl.AddCar(car3);
+
+        Collection cars = carImpl.getAllCars();
+        
+        List<Car> rightCars = new ArrayList<>();
+        rightCars.add(car1);
+        rightCars.add(car2);
+        rightCars.add(car3);
+        
+        Assert.assertNotSame(cars, rightCars);
+        Assert.assertTrue("Missing expected user. ", cars.containsAll(rightCars));   
+    }
+    
+    @Test
+    public void getRentedCarsTest(){
+        
+    }
+    
+    @Test
+    public void getFreeCarsTest(){
+        
+    }
     
 }
