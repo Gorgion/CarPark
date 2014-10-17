631d2591cca9a38d66ed874d3999681ffe1b9617
Gorgion <svoboda.tomas.01@gmail.com>
2014-10-17 13:17:16 +0200

    user DAO update

--- carpark-backend/src/main/java/cz/muni/fi/pa165/carpark/dao/UserDao.java
+++ carpark-backend/src/main/java/cz/muni/fi/pa165/carpark/dao/UserDao.java
@@ -3,79 +3,65 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cz.muni.fi.pa165.carpark.dao;
 
 import cz.muni.fi.pa165.carpark.entity.User;
 import java.util.List;
-import javax.persistence.EntityManagerFactory;
 
 /**
  * CRUD operations on User entity.
- * 
+ *
  * @author Tomáš Vašíček
  */
-
-//TODO propagate changes to class diagram
-public interface UserDao 
-{   
+public interface UserDao
+{
     /**
      * Persist new user
-     * 
+     *
      * @param user user for persitance
      */
     void add(User user);
-    
+
     /**
      * Get user by given id.
-     * 
+     *
      * @param id id of searched user
-     * @return 
+     * @return
      */
     User get(Long id);
-    
+
     /**
      * Edit given user.
-     * 
+     *
      * @param user user for editing
      */
     void edit(User user);
-    
-    /**
-     * Delete given user
-     * 
-     * @param user user for deletion
-     */
-    void delete(User user);    
 
     /**
-     *  Get all users
-     * 
+     * Delete given user
+     *
+     * @param user user for deletion
+     */
+    void delete(User user);
+
+    /**
+     * Get all users
+     *
      * @return List<User> of users
      */
     List<User> getAll();
-    
+
     /**
-     *  Get all users with rent
-     * 
-     * @return  List<User>of users
+     * Get all users with rent
+     *
+     * @return List<User>of users
      */
     List<User> getAllWithRent();
-    
+
     /**
-     *  Get all users without rent
-     * 
+     * Get all users without rent
+     *
      * @return List<User> of users
      */
     List<User> getAllWithoutRent();
-    
-    /**
-     * Setter for entity manager factory.
-     * 
-     * @param emf Given entity manager factory .
-     */
-    void setEmf(EntityManagerFactory entityManagerFactory);
-    
-    
-    
 }
--- carpark-backend/src/main/java/cz/muni/fi/pa165/carpark/dao/UserDaoImpl.java
+++ carpark-backend/src/main/java/cz/muni/fi/pa165/carpark/dao/UserDaoImpl.java
@@ -3,166 +3,117 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cz.muni.fi.pa165.carpark.dao;
 
 import cz.muni.fi.pa165.carpark.entity.User;
 import java.util.List;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
-import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 
 /**
  * CRUD operations on User entity.
- * 
+ *
  * @author Tomáš Vašíček
  */
-public class UserDaoImpl implements UserDao {
-    
-    //@PersistenceContext
-    //private EntityManager entityManager;
-    private EntityManagerFactory entityManagerFactory;
-    
+@Named
+public class UserDaoImpl implements UserDao
+{
+
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
+
     @Override
-    public void setEmf(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
-    }
-    
-    @Override
-    public void add(User user) {
-        if (user == null) {
+    public void add(User user)
+    {
+        if (user == null)
+        {
             throw new NullPointerException("User can not be NULL");
         }
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(user);
-            entityManager.getTransaction().commit();
-        } catch (PersistenceException ex) {
-            entityManager.getTransaction().rollback();
-            ex.printStackTrace();
-        }
-        finally{
-            entityManager.close();
-        }        
+
+        entityManager.persist(user);
     }
 
     @Override
-    public User get(Long id) {
-        if (id == null){
+    public User get(Long id)
+    {
+        if (id == null)
+        {
             throw new NullPointerException("ID can not be NULL");
         }
-        if(id < 0) {
+        if (id < 0)
+        {
             throw new IllegalArgumentException("ID is less than 0");
         }
-        
-        User user = null;
-        EntityManager entityManager = entityManagerFactory.createEntityManager();     
-        try{
-            user = entityManager.find(User.class, id);
-        }
-        catch (PersistenceException ex){
-            ex.printStackTrace();
-        }
-        finally{
-            entityManager.close();
-        } 
-        
-//        if (user == null) {
-//            throw new IllegalArgumentException("User is not found");
-//        }
+
+        User user = entityManager.find(User.class, id);
+
         return user;
     }
 
     @Override
-    public void edit(User user) {
-        if (user == null) {
+    public void edit(User user)
+    {
+        if (user == null)
+        {
             throw new NullPointerException("User can not be NULL");
         }
-        
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try{
-            entityManager.getTransaction().begin();
-            entityManager.merge(user);
-            entityManager.getTransaction().commit();
-        }
-        catch(PersistenceException ex){
-            if (entityManager.getTransaction().isActive()) {
-                entityManager.getTransaction().rollback();
-            }
-            ex.printStackTrace();
-        }
-        finally{
-            entityManager.close();
-        } 
+
+        entityManager.merge(user);
     }
 
     @Override
-    public void delete(User user) {
-        if (user == null) {
+    public void delete(User user)
+    {
+        if (user == null)
+        {
             throw new NullPointerException("User can not be NULL");
         }
-        
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        try{
-            User userToDelete = get(user.getId());
-            //User userToDelete = entityManager.find(User.class, user.getId());
-            
-            entityManager.getTransaction().begin();
-            entityManager.remove(entityManager.merge(userToDelete));
-            entityManager.getTransaction().commit();
-        }
-        catch(PersistenceException ex){
-            entityManager.getTransaction().rollback();
-            ex.printStackTrace();
-        }
-        finally{
-            entityManager.close();
-        } 
+
+        User userToDelete = entityManager.find(User.class, user.getId());
+
+        entityManager.remove(userToDelete);
     }
 
     @Override
-    public List<User> getAll() {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        
+    public List<User> getAll()
+    {
+
         Query query = entityManager.createQuery(
-                "SELECT u FROM User u", User.class);;
+                "SELECT u FROM User u", User.class);
         List<User> users = query.getResultList();
-        entityManager.close();
-        
+
         return users;
     }
 
     @Override
-    public List<User> getAllWithRent() {    
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        
-        //TODO edit naive version
+    public List<User> getAllWithRent()
+    {
+
         Query query = entityManager.createQuery(
                 "SELECT u FROM User u WHERE u.id IN (SELECT DISTINCT r.user FROM Rental r)", User.class);
-                //"SELECT u FROM User u WHERE u.id IN (SELECT r.user, r.rentalState from Rental r WHERE r.rentalState := state", User.class);
+        //"SELECT u FROM User u WHERE u.id IN (SELECT r.user, r.rentalState from Rental r WHERE r.rentalState := state", User.class);
         //query.setParameter("state", State.APPROVED);
         List<User> users = query.getResultList();
-        entityManager.close();
-        
+
         return users;
     }
 
     @Override
-    public List<User> getAllWithoutRent() {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        
+    public List<User> getAllWithoutRent()
+    {
+
         //TODO edit naive version
         Query query = entityManager.createQuery(
                 "SELECT u FROM User u WHERE u.id NOT IN (SELECT DISTINCT r.user FROM Rental r)", User.class);
-                //"SELECT u FROM User u WHERE u.id NOT IN (SELECT r.user, r.rentalState from Rental r WHERE r.rentalState := state", User.class);
+        //"SELECT u FROM User u WHERE u.id NOT IN (SELECT r.user, r.rentalState from Rental r WHERE r.rentalState := state", User.class);
         //query.setParameter("state", State.APPROVED);
         List<User> users = query.getResultList();
-        entityManager.close();
-        
+
         return users;
     }
-    
+
 }
