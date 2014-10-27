f23ffe080380ba0420b7cea5cc21d238156c65b3
KB9 <karolinaburska9@gmail.com>
2014-10-27 11:57:21 +0100

    OfficeService completed

---
+++ carpark-service/src/main/java/cz/muni/fi/pa165/carpark/service/OfficeService.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.carpark.service;
+
+import cz.muni.fi.pa165.carpark.dto.CarDto;
+import cz.muni.fi.pa165.carpark.dto.OfficeDto;
+import cz.muni.fi.pa165.carpark.dto.UserDto;
+import java.util.List;
+
+/**
+ *
+ * @author Lorain
+ */
+public interface OfficeService {
+    /**
+         * Add new office
+         * 
+         * @param office new office to be added 
+         */
+        public void addOffice(OfficeDto office);
+
+        /**
+         * Get existing office
+         * 
+         * @param id id of existing office
+         * @return office with the id specified
+         */
+	public OfficeDto getOffice(Long id);
+
+        /**
+         * Edit given office
+         * 
+         * @param office office for editing
+         */
+	public void editOffice(OfficeDto office);
+
+        /**
+         * Delete given office
+         * 
+         * @param office office for removal
+         */
+	public void deleteOffice(OfficeDto office);
+
+        /**
+         * Return all offices
+         * 
+         * @return List of all offices
+         */
+	public List<OfficeDto> getAllOffices();
+
+        /**
+         * Get all cars from specific office
+         * 
+         * @param office office from which cars are returned
+         * @return List of all cars from one office
+         */
+	public List<CarDto> getOfficeCars(OfficeDto office);
+        
+        /**
+         * Adds a new car to some office
+         * 
+         * @param office office into which a car should be added
+         * @param car car for the office
+         */
+        public void addCarToOffice(OfficeDto office, CarDto car);
+
+        /**
+         * Deletes specified car from some office
+         * 
+         * @param office office from which a car is to be deleted
+         * @param car car for deletion
+         */
+        public void deleteCarFromOffice(OfficeDto office, CarDto car);
+        
+        /**
+         * Get all employees from specified office
+         * 
+         * @param office office from which employees are returned
+         * @return List of all employees from one office
+         */
+	public List<UserDto> getEmployees(OfficeDto office);
+        
+        /**
+         * Adds specified user into some office
+         * 
+         * @param office office into which an user is to be added
+         * @param user user for the office
+         */
+        public void addEmployeeToOffice(OfficeDto office, UserDto user);
+        
+        /**
+         * Deletes specified user from some office
+         * 
+         * @param office office from which an user is to be deleted
+         * @param user user for deletion
+         */
+        public void deleteEmployeeFromOffice(OfficeDto office, UserDto user);
+
+}
---
+++ carpark-service/src/main/java/cz/muni/fi/pa165/carpark/service/OfficeServiceImpl.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.carpark.service;
+
+import cz.muni.fi.pa165.carpark.dao.OfficeDao;
+import cz.muni.fi.pa165.carpark.dto.CarDto;
+import cz.muni.fi.pa165.carpark.dto.OfficeDto;
+import cz.muni.fi.pa165.carpark.dto.UserDto;
+import cz.muni.fi.pa165.carpark.entity.Car;
+import cz.muni.fi.pa165.carpark.entity.Office;
+import cz.muni.fi.pa165.carpark.entity.User;
+import cz.muni.fi.pa165.carpark.util.Converter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import org.springframework.dao.DataAccessException;
+
+/**
+ *
+ * @author Karolina Burska
+ */
+public class OfficeServiceImpl implements OfficeService {
+
+    //@Inject
+    private OfficeDao officeDao;
+
+    public void setRentalDao(OfficeDao office) {
+        officeDao = office;
+    }
+    
+    @Transactional
+    @Override
+    public void addOffice(OfficeDto office) {
+        try {
+            Office officeEntity = Converter.getOfficeEntity(office);
+            officeDao.addOffice(officeEntity);
+        } 
+        catch (Exception e) {
+            throw new DataAccessException("Error occurred while office entity adding."){};
+        }
+    }
+
+    @Transactional
+    @Override
+    public OfficeDto getOffice(Long id) {
+        try {
+            Office officeEntity = officeDao.getOffice(id);
+            return Converter.getTDOOffice(officeEntity);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while getting office.") {};
+        }
+    }
+
+    @Transactional
+    @Override
+    public void editOffice(OfficeDto office) {
+        try {
+            Office officeEntity = Converter.getOfficeEntity(office);
+            officeDao.editOffice(officeEntity);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while editing office.") {};
+        }
+    }
+
+    @Transactional
+    @Override
+    public void deleteOffice(OfficeDto office) {
+        try {
+            Office officeEntity = Converter.getOfficeEntity(office);
+            officeDao.deleteOffice(officeEntity);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while deleting office.") {};
+        }
+    }
+
+    @Transactional
+    @Override
+    public List<OfficeDto> getAllOffices() {
+        try {
+            List<OfficeDto> officesDto = new ArrayList<>();
+            List<Office> offices = new ArrayList<>(officeDao.getAllOffices());
+            for (Office office : offices) {
+                officesDto.add(Converter.getTDOOffice(office));
+            }
+            
+            return Collections.unmodifiableList(officesDto);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while getting a list of offices.") {};
+        }
+    }
+
+    @Transactional
+    @Override
+    public List<CarDto> getOfficeCars(OfficeDto office) {
+        try {
+            List<CarDto> officeCarsDto = new ArrayList<>();
+            List<Car> cars = new ArrayList<>(officeDao.getOfficeCars(officeDao.getOffice(office.getID())));
+            for (Car car : cars) {
+                officeCarsDto.add(Converter.getTransferObject(car));
+            }
+            
+            return Collections.unmodifiableList(officeCarsDto);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while getting a list of office cars.") {};
+        }
+    }
+
+    @Transactional
+    @Override
+    public void addCarToOffice(OfficeDto office, CarDto car) {
+        try {
+            Office officeEntity = Converter.getOfficeEntity(office);
+            Car carEntity = Converter.getEntity(car);
+            officeDao.addCarToOffice(officeEntity, carEntity);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while adding car into office.") {};
+        }  
+    }
+
+    @Transactional
+    @Override
+    public void deleteCarFromOffice(OfficeDto office, CarDto car) {
+        try {
+            Office officeEntity = Converter.getOfficeEntity(office);
+            Car carEntity = Converter.getEntity(car);
+            officeDao.deleteCarFromOffice(officeEntity, carEntity);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while adding car into office.") {};
+        }  
+    }
+
+    @Transactional
+    @Override
+    public List<UserDto> getEmployees(OfficeDto office) {
+        try {
+            List<UserDto> officeEmployeesDto = new ArrayList<>();
+            List<User> employees = new ArrayList<>(officeDao.getEmployees(officeDao.getOffice(office.getID())));
+            for (User employee : employees) {
+                officeEmployeesDto.add(Converter.getTransferObject(employee));
+            }
+            
+            return Collections.unmodifiableList(officeEmployeesDto);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while getting a list of office employees.") {};
+        }
+    }
+
+    @Transactional
+    @Override
+    public void addEmployeeToOffice(OfficeDto office, UserDto user) {
+        try {
+            Office officeEntity = Converter.getOfficeEntity(office);
+            User userEntity = Converter.getEntity(user);
+            officeDao.addEmployeeToOffice(officeEntity, userEntity);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while adding car into office.") {};
+        }  
+    }
+
+    @Transactional
+    @Override
+    public void deleteEmployeeFromOffice(OfficeDto office, UserDto user) {
+        try {
+            Office officeEntity = Converter.getOfficeEntity(office);
+            User userEntity = Converter.getEntity(user);
+            officeDao.deleteEmployeeFromOffice(officeEntity, userEntity);
+        }
+        catch(Exception e) {
+            throw new DataAccessException("Error occurred while adding car into office.") {};
+        } 
+    }
+    
+}
