032d153d1515786cae1f3795974142bccb541e78
Vasino <vas1k.cz@gmail.com>
2014-11-26 18:26:47 +0100

    user web stuff - 53.9% completed

--- carpark-web/src/main/java/cz/muni/fi/pa165/carpark/web/controller/UserController.java
+++ carpark-web/src/main/java/cz/muni/fi/pa165/carpark/web/controller/UserController.java
@@ -12,11 +12,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import javax.validation.Valid;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import org.aspectj.util.LangUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,6 +30,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * User controller.
@@ -35,47 +38,72 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  * @author Tomas Vasicek
  */
 @Controller
-@RequestMapping("/auth/user/{userId}/user")
-public class UserController
-{
+@RequestMapping("/auth/user")
+public class UserController {
     @Autowired
     private UserService userService;
-
-    @ModelAttribute("userId")
-    public Long getUserId(@PathVariable Long userId)
-    {
-        return userId;
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String list(@PathVariable(value = "userId") Long userId, Model model)
-    {
-        List<UserDto> users = userService.getAll();
-        model.addAttribute("users", users);
-        return "user-list";
-    }
     
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    @Autowired
+    private MessageSource messageSource;
+
+    //@ModelAttribute("user-form")
+    //@RequestMapping(value = "/add", method = RequestMethod.GET)
     public UserForm getNewForm() {
         return new UserForm();
     }
     
-    @RequestMapping(value = "/{id}/delete", method =
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(Model model)
+    { 
+//        UserDto userDto = new UserDto();
+//        userDto.setFirstName("Name");
+//        userDto.setLastName("Surname");
+//        userDto.setAddress("Address");
+//        userDto.setBirthNumber("958456/8524");
+//        userService.add(userDto);
+        List<UserDto> users = userService.getAll();
+        model.addAttribute("users", users);
+        model.addAttribute("userForm", new UserForm());
+        return "user-list";
+    }
+    
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String addRequest(Model model)
     {
-        RequestMethod.POST, RequestMethod.DELETE
-    })
-    public String userDeleted(@PathVariable Long userId, @PathVariable Long id, /*@Valid @ModelAttribute RentalDto rental, BindingResult result,*/ RedirectAttributes redirectAttributes)
+        //System.out.println("On Submit");
+        model.addAttribute("userForm", new UserForm());
+        return "user-form";
+    }
+    
+    
+    @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+    public String editRental(@PathVariable Long id, Model model)
     {
         UserDto user = userService.get(id);
-        if (user == null)
-        {
-            redirectAttributes.addFlashAttribute("error", "error.user.deleted");
-        }
-        userService.delete(user);
-        redirectAttributes.addFlashAttribute("msg", "msg.user.deleted");
         
-        return "redirect:/auth/user/" + userId + "/user";
+        UserForm userForm = new UserForm();
+        userForm.setFirstName(user.getFirstName());
+        userForm.setLastName(user.getLastName());
+        userForm.setAddress(user.getAddress());
+        userForm.setBirthNumber(user.getBirthNumber());
+        
+        model.addAttribute("userForm", userForm);
+        model.addAttribute("action", "edit");
+        
+        return "user-form";
     }
+        
+    
+    @RequestMapping(value = "/{id}/delete", method = { RequestMethod.POST, RequestMethod.DELETE })
+    public String delete(@PathVariable long id, RedirectAttributes redirectAttributes, Locale locale, UriComponentsBuilder uriBuilder) {
+        UserDto user = userService.get(id);
+        userService.delete(user);
+        redirectAttributes.addFlashAttribute(
+                "message",
+                messageSource.getMessage("user.delete.message", new Object[]{ user.getId(), user.getFirstName(), user.getLastName(), user.getBirthNumber(), user.getAddress()}, locale)
+        );
+        return "redirect:" + uriBuilder.path("/auth/user").build();
+    }    
     
     public static class UserForm
     {
--- carpark-web/src/main/webapp/WEB-INF/views/user-form.jsp
+++ carpark-web/src/main/webapp/WEB-INF/views/user-form.jsp
@@ -11,7 +11,7 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
-<fmt:message var="title" key="rental.form.title"/>
+<fmt:message var="title" key="user.form.title"/>
 
 
 
@@ -34,5 +34,60 @@
                 <fmt:message key="${error}"/>
             </div>
         </c:if>
+        
+        <c:url var="addUserUrl" value="/auth/user/add" />
+        <form:form action="${addUserUrl}" method="POST" modelAttribute="userForm" class="form-horizontal">
+            <div class="form-group col-sm-5"> 
+                <form:label path="firstName"><fmt:message key="user.firstName" /></form:label>
+                <form:input path="firstName" cssClass="form-control col-sm-8" />
+            </div>
+          <div class="form-group col-sm-5"> 
+                <form:label path="lastName"><fmt:message key="user.lastName" /></form:label>
+                <form:input path="lastName" cssClass="form-control" />
+            </div>
+          <div class="form-group col-sm-5"> 
+                <form:label path="birthNumber"><fmt:message key="user.birthNumber" /></form:label>
+                <form:input path="birthNumber" cssClass="form-control" />
+            </div>
+          <div class="form-group col-sm-5"> 
+                <form:label path="address"><fmt:message key="user.address" /></form:label>
+                <form:input path="address" cssClass="form-control" />
+            </div>
+           
+            <button type="submit" class="btn btn-success"><fmt:message key="user.add" /></button>
+        </form:form>
+
+        <form class="form-horizontal" method="post" action='employee/add.htm' name="userForm" id="userForm">
+            <div class="control-group">
+                <label class="control-label">Firstname</label>
+                <div class="controls">
+                    <input type="text" name="firstName" id="firstName" title="First Name" path="firstName">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">Lastname</label>
+                <div class="controls">
+                    <input type="text" name="lastName" id="lastName" title="Last Name" value="">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">Birth number</label>
+                <div class="controls">
+                    <input type="text" name="email" id="email" title="Email" value="">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">Address</label>
+                <div class="controls">
+                    <input type="text" name="email" id="email" title="Email" value="">
+                </div>
+            </div>
+            <br />
+            <div class="form-actions">
+                <button type="submit" class="btn btn-success">Submit</button>
+                <button type="button" class="btn">Cancel</button>
+            </div>
+        </form>
+
     </jsp:attribute>
 </custom:layout>
