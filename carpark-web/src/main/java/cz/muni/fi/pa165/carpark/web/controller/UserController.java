41ee263efd901710780109b5a4e85f4c78664d3a
Vasino <vas1k.cz@gmail.com>
2014-11-27 17:23:08 +0100

    user web stuff - 58.9% completed (form validation, controller)

--- carpark-web/src/main/java/cz/muni/fi/pa165/carpark/web/controller/UserController.java
+++ carpark-web/src/main/java/cz/muni/fi/pa165/carpark/web/controller/UserController.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Locale;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -36,26 +38,29 @@ public class UserController {
     @RequestMapping(method = RequestMethod.GET)
     public String list(Model model) {
         List<UserDto> users = userService.getAll();
-        
+
         model.addAttribute("users", users);
         model.addAttribute("userForm", new UserForm());
-        
+
         return "user-list";
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addRequest(Model model) {
-        model.addAttribute("userForm", new UserForm()); 
+        model.addAttribute("userForm", new UserForm());
         return "user-form";
     }
 
-    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
-    public String addedUser(@Valid @ModelAttribute UserForm userForm, BindingResult bindingResult, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder, Locale locale) {
-        UserDto user = getUserDto(userForm);   
-        userService.add(user);
-        
-        redirectAttributes.addFlashAttribute("msg", messageSource.getMessage("msg.user.created", null, locale));
-        return "redirect:" + uriBuilder.path("/auth/user").build();
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addedUser(@Valid @ModelAttribute UserForm userForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "user-form";
+        } else {
+            UserDto user = getUserDto(userForm);
+            userService.add(user);
+            redirectAttributes.addFlashAttribute("msg", "msg.user.created");
+            return "redirect:/auth/user";
+        }
     }
 
     @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
@@ -69,73 +74,85 @@ public class UserController {
         return "user-form";
     }
 
-
     @RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public String editedUser(@Valid @ModelAttribute UserForm userForm, BindingResult bindingResult, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder, Locale locale) {
-        UserDto user = userService.get(userForm.getId());
-        
-        user.setAddress(userForm.getAddress());
-        user.setBirthNumber(userForm.getBirthNumber());
-        user.setFirstName(userForm.getFirstName());
-        user.setLastName(userForm.getLastName());
-        
-        userService.edit(user);
-        
-        redirectAttributes.addFlashAttribute("msg", messageSource.getMessage("msg.user.edited", null, locale));
-        
-        return "redirect:" + uriBuilder.path("/auth/user").build();
+    public String editedUser(@Valid @ModelAttribute UserForm userForm, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("action", "edit");
+            return "user-form";
+        } else {
+            UserDto user = userService.get(userForm.getId());
+
+            if (user == null) {
+                redirectAttributes.addFlashAttribute("error", "error.user.edited");
+                return "redirect:/auth/user";
+            }
+            user.setAddress(userForm.getAddress());
+            user.setBirthNumber(userForm.getBirthNumber());
+            user.setFirstName(userForm.getFirstName());
+            user.setLastName(userForm.getLastName());
+
+            userService.edit(user);
+
+            redirectAttributes.addFlashAttribute("msg", "msg.user.edited");
+            return "redirect:/auth/user";
+        }
+
     }
 
     @RequestMapping(value = "/{id}/delete", method = {RequestMethod.POST, RequestMethod.DELETE})
-    public String delete(@PathVariable long id, RedirectAttributes redirectAttributes, Locale locale, UriComponentsBuilder uriBuilder) {
-        UserDto user = userService.get(id);
-        if (user == null) {
-            redirectAttributes.addFlashAttribute("error", "error.user.deleted");
-        } else {
+    public String delete(@PathVariable long id, RedirectAttributes redirectAttributes) {
+        UserDto user;
+        try {
+            user = userService.get(id);
+            if (user == null) {
+                redirectAttributes.addFlashAttribute("error", "error.user.deleted");
+                return "redirect:/auth/user";
+            }
             userService.delete(user);
-            redirectAttributes.addFlashAttribute("msg", "msg.user.deleted");
+        } catch (IllegalArgumentException | DataAccessException ex) {
+            redirectAttributes.addFlashAttribute("error", "error.user.deleted" + ex);
+            return "redirect:/auth/user";
         }
 
-        return "redirect:" + uriBuilder.path("/auth/user").build();
+        redirectAttributes.addFlashAttribute("msg", "msg.user.deleted");
+        return "redirect:/auth/user";
     }
-    
-    
+
     private UserDto getUserDto(UserForm userForm) {
         UserDto user = new UserDto();
-        
+
         user.setId(userForm.getId());
         user.setAddress(userForm.getAddress());
         user.setBirthNumber(userForm.getBirthNumber());
         user.setFirstName(userForm.getFirstName());
         user.setLastName(userForm.getLastName());
-        
+
         return user;
     }
-    
+
     private UserForm getUserForm(UserDto user) {
         UserForm userForm = new UserForm();
-        
+
         userForm.setId(user.getId());
         userForm.setFirstName(user.getFirstName());
         userForm.setLastName(user.getLastName());
         userForm.setAddress(user.getAddress());
         userForm.setBirthNumber(user.getBirthNumber());
-        
+
         return userForm;
     }
 
     public static class UserForm {
 
-        @NotNull
         private Long id;
 
-        @NotNull
+        @NotEmpty
         private String firstName;
 
-        @NotNull
+        @NotEmpty
         private String lastName;
 
-        @NotNull
+        @NotEmpty
         private String birthNumber;
 
         private String address;
